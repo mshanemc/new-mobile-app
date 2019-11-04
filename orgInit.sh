f0e72ca83010d5a7ac155495f56b90417cf4c773
mshanemc <shane.mclaughlin@salesforce.com>
2019-11-04 15:26:09 -0600

    friendlier password/username

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix mobile -o work.shop
 sfdx force:source:push
+sfdx shane:user:password:set -g User -l User -p mobapp19
 sfdx force:org:open
